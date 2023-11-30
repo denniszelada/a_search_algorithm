f580b747bd050884a2e47d464e9c9bd25f12e2fd
Dennis Zelada <denniszelada@gmail.com>
2023-11-30 20:34:45 +0100

    Add function ReadBoardFile to read the board from a file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
+
 using std::cout;
 using std::vector;
+using std::string;
+using std::ifstream;
 
+void ReadBoardFile(string board) {
+  ifstream my_file;
+  my_file.open(board);
+  if (my_file) {
+    string line;
+    while (getline(my_file, line)) {
+      cout << line << "\n";
+    }
+  }
+}
 
-void PrintBoard(const vector<vector<int>> &board)
+void PrintBoard(const vector<vector<int> > &board)
 {
 	for (int i = 0; i < board.size(); i++) {
     	for (int j=0; j < board[0].size(); j++) {
@@ -15,10 +30,11 @@ void PrintBoard(const vector<vector<int>> &board)
 }
 
 int main() {
-  vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 1, 0}};
-  PrintBoard(board);
+  // vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+  //                           {0, 1, 0, 0, 0, 0},
+  //                           {0, 1, 0, 0, 0, 0},
+  //                           {0, 1, 0, 0, 0, 0},
+  //                           {0, 0, 0, 0, 1, 0}};
+  // PrintBoard(board);
+  ReadBoardFile("1.board");
 }
